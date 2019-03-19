b46a560349c6391566fdeff4c992a5ca1ec74b3e
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-03-19 14:28:48 +0100

    View class inital commit

---
+++ src/com/codecool/View.java
@@ -0,0 +1,7 @@
+package com.codecool;
+
+public class View {
+    public static void personCreationMessage(int floor, int destinationFloor){
+        System.out.println("Created person on "+floor+" floor with destination floor "+destinationFloor);
+    }
+}
