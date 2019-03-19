9d95fe045aa7a4235cda6f5635a24fe3100ebc22
Karol Karpeta <karol.karpeta@gmail.com>
2019-03-19 13:17:12 +0100

    Building constructor created

--- src/com/codecool/Building.java
+++ src/com/codecool/Building.java
@@ -4,6 +4,11 @@ public class Building {
     private final Floor[] floors;
     private final Elevator[] elevators;
 
+    public Building(Floor[] floors, Elevator[] elevators) {
+        this.floors = floors;
+        this.elevators = elevators;
+    }
+
     private void moveElevator(Floor destination, Elevator elevator) {
     }
 }
--- src/com/codecool/Floor.java
+++ src/com/codecool/Floor.java
@@ -1,5 +1,7 @@
 package com.codecool;
 
+import java.util.LinkedList;
+
 public class Floor {
     private LinkedList<Person> upQueue;
     private LinkedList<Person> downQueue;
