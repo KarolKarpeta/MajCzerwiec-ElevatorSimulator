8d51f9db627b8dd9d7e997067ec6363f290e4309
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-03-20 10:56:12 +0100

    removed needless things

--- src/com/codecool/Building.java
+++ src/com/codecool/Building.java
@@ -1,7 +1,6 @@
 package com.codecool;
 
 import java.util.Random;
-import java.util.LinkedList;
 
 public class Building {
     private final Floor[] floors;
@@ -63,11 +62,9 @@ public class Building {
 
     public static void main(String[] args) {
         Building kolejowa5na7 = createBuilding(8, 1);
-        kolejowa5na7.generatePerson();
-        System.out.println(getBuilding());
         SpawnPeople spawnPeople = new SpawnPeople();
         (new Thread(spawnPeople)).start();
-
+        System.out.println("Coś");
     }
 
 }
