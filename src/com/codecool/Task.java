083edf0630c0686b5b87addae303b4bbf807dd1f
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-03-20 11:31:42 +0100

    Task constructor done

--- src/com/codecool/Task.java
+++ src/com/codecool/Task.java
@@ -3,4 +3,9 @@ package com.codecool;
 public class Task {
     int destinationFloorNumber;
     int startFloorNumber;
+
+    public Task(int startFloorNumber, int destinationFloorNumber){
+        this.startFloorNumber = startFloorNumber;
+        this.destinationFloorNumber = destinationFloorNumber;
+    }
 }
