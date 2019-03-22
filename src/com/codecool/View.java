13afb7f43038571ef39df9f8ae3c16eddd3e06b0
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-03-22 13:50:29 +0100

    view programisty 1.0

--- src/com/codecool/View.java
+++ src/com/codecool/View.java
@@ -2,21 +2,41 @@ package com.codecool;
 
 public class View {
 
+
+    public static void print(Task task){
+        System.out.print("["+task.getDestinationFloorNumber());
+        if(task.hasToLoad()){
+            System.out.print(", load], ");
+        }else{
+            System.out.print(", unload], ");
+        }
+    }
+
+    public static void print(Person[] people){
+        System.out.print("People:");
+        for(Person person : people){
+            System.out.print(" " + person.getDestinationFloor());
+        }
+        System.out.println();
+    }
+
+    public static void showElevator(Elevator elevator){
+        System.out.println(elevator.getName() + " on " + elevator.getFloor().getFloorNumber()+" floor");
+        System.out.print("Tasks: ");
+        for(Task task : elevator.getTasks()){
+            print(task);
+        }
+        System.out.println();
+        print(elevator.getPeople());
+        System.out.println("#############################################################");
+    }
+
     public static void buildingCreationMessage(int nrOfFloors, int nrOfElevators) {
-        System.out.println("Created building with " + nrOfFloors + " floors and " + nrOfElevators + " elevators. Here the game begins...");
+        System.out.println("Building: " + nrOfFloors + " floors and " + nrOfElevators + " elevators");
     }
 
     public static void personCreationMessage(int floor, int destinationFloor) {
         System.out.println("Created person " + floor + " => " + destinationFloor);
     }
 
-    public static void confirmTaskAssignmentToElevator(Elevator elevator, Task task) {
-        System.out.print(elevator.getName() + " on " + elevator.getFloor().getFloorNumber() +
-                " floor, received a task [" + task.getDestinationFloorNumber() + ", ");
-        if(task.hasToLoad()){
-            System.out.println("load]");
-        }else{
-            System.out.println("unload]");
-        }
-    }
 }
