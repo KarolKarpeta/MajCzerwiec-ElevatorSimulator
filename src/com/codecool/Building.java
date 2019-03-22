e382146281bc47e2bd413f47d24cf734bd5f5933
Karol Karpeta <karol.karpeta@gmail.com>
2019-03-22 10:52:16 +0100

    begin of console view creation

--- src/com/codecool/Building.java
+++ src/com/codecool/Building.java
@@ -32,6 +32,8 @@ public class Building {
             elevators[i] = new Elevator(floors[0], i);
             (new Thread(elevators[i])).start();
         }
+
+        View.buildingCreationMessage(nrOfFloors, nrOfElevators);
     }
 
     public Floor getLowerFloor(Floor floor) {
@@ -106,12 +108,12 @@ public class Building {
             theChosenElevator = getElevatorWithSmallestNumberOfTasks();
         }
         theChosenElevator.addTask(task);
-        View.showElevator(theChosenElevator);
+        View.confirmTaskAssignmentToElevator(theChosenElevator, task);
     }
 
     public static void main(String[] args) {
         Building kolejowa5na7 = createBuilding(8, 2);
-        SpawnPeople spawnPeople = new SpawnPeople();
-        (new Thread(spawnPeople)).start();
+        PeopleSpawner peopleSpawner = new PeopleSpawner();
+        (new Thread(peopleSpawner)).start();
     }
 }
