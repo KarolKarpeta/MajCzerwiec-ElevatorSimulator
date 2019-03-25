b5002242a86f41589553e7b4197bd4b8c0855123
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-03-25 12:14:04 +0100

    merge Person class; some improvements; functions are sorted alphabetically

--- src/com/codecool/Person.java
+++ src/com/codecool/Person.java
@@ -1,16 +1,35 @@
 package com.codecool;
 
+import java.util.Random;
+
 public class Person {
     private final int destinationFloor;
-
-    public Person(int destinationFloor) {
-        this.destinationFloor = destinationFloor;
-    }
+    private final String name;
+    private static String[] names = {"Maciek", "Józek", "Antek", "Janek", "Krzychu", "Marian", "Ferdek", "Walduś", "Halina", "Grażyna", "Hela",
+            "Jaćka", "Heniek", "Stasek", "Franek", "Zbynek", "Zenek", "Brygida", "Kuba", "Klemens", "Eustachy", "Edward",
+            "Eugeniusz", "Fryderyk", "Gniewomir", "Gustaw", "Gżegoż", "Irek", "Jerzy", "Julian", "Kajetan", "Konstanty",
+            "Kryspin", "Krzesimir", "Kondrad", "Kazek", "Leopold", "Leszek", "Leopold", "Maurycy", "Mieszko", "Arek", "Karol"};
 
     public int getDestinationFloor() {
         return destinationFloor;
     }
-    public Task getUnloadingTask(){
+
+    public String getName() {
+        return name;
+    }
+
+    private String getRandomName() {
+        Random random = new Random();
+        return names[random.nextInt(names.length)];
+    }
+
+    public Task getUnloadingTask() {
         return new Task(this);
+    }
+
+    public Person(int destinationFloor) {
+        this.destinationFloor = destinationFloor;
+        this.name = getRandomName();
+
     }
 }
