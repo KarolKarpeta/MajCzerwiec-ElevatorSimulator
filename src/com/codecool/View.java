98454ee112db89db2aff646ac57fbef46a86f1a6
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-03-26 13:40:31 +0100

    View: both user-friendly and programisty

--- src/com/codecool/View.java
+++ src/com/codecool/View.java
@@ -8,11 +8,11 @@ public class View {
         System.out.println("Building: " + nrOfFloors + " floors and " + nrOfElevators + " elevators. Here the game begins...\n");
     }
 
-    public static void confirmTaskAssignmentToElevator(Elevator elevator, Task task){
+    public static void confirmTaskAssignmentToElevator(Elevator elevator, Task task) {
         if (task.hasToLoad()) {
-        System.out.println("Elevator: " + elevator.getName() + " on floor " + elevator.getFloor().getFloorNumber() +
-                ", task => " + task.getDestinationFloorNumber() +
-                " to take a person.");
+            System.out.println("Elevator: " + elevator.getName() + " on floor " + elevator.getFloor().getFloorNumber() +
+                    ", task => " + task.getDestinationFloorNumber() +
+                    " to take a person.");
         } else {
             System.out.println("Elevator:" + elevator.getName() + " loads person on floor " + elevator.getFloor().getFloorNumber() +
                     "and is to transport a person to " + task.getDestinationFloorNumber() +
@@ -20,60 +20,68 @@ public class View {
         }
     }
 
-    public static void showFloors(){
-        Building kolejowa = Building.getBuilding();
-        Floor[] floors = kolejowa.getFloors();
-        for (int i = floors.length-1; i >= 0; i--) {
-            for(Elevator elevator : kolejowa.getElevators()){
-                if(elevator.getFloor().getFloorNumber() == floors[i].getFloorNumber())
-                System.out.print("#");
-            }
-            System.out.println("\tFloor "+floors[i].getFloorNumber()+" Up:"+floors[i].getUpQueue()
-            +" Down:"+floors[i].getDownQueue()+" Transported: "+floors[i].getTransportedPeople());
-        }
+    public static void personCreationMessage(int floor, int destinationFloor) {
+        System.out.println("Created person " + floor + " => " + destinationFloor);
     }
 
-    public static void print(Task task){
-        if(task == null){
+    public static void personCreationMessage(int floor, Person person) {
+        System.out.println("Created person " + person.getName() + " on " + floor + " floor with destination floor " + person.getDestinationFloor() + ".");
+    }
+
+    public static void personLoadMessage(Elevator elevator, Person person) {
+        System.out.println("Person " + person.getName() + " onboarded on floor " + elevator.getFloor().getFloorNumber() + " by " + elevator.getName() + ".");
+    }
+
+    public static void print(Task task) {
+        if (task == null) {
             System.out.print("null");
             return;
         }
-        System.out.print("["+task.getDestinationFloorNumber());
-        if(task.hasToLoad()){
+        System.out.print("[" + task.getDestinationFloorNumber());
+        if (task.hasToLoad()) {
             System.out.print(", load], ");
-        }else{
+        } else {
             System.out.print(", unload], ");
         }
     }
 
-    public static void print(Person[] people){
+    public static void print(Person[] people) {
         System.out.print("People:");
-        for(Person person : people){
+        for (Person person : people) {
             System.out.print(" " + person.getDestinationFloor());
         }
         System.out.println();
     }
 
-    public static void showElevator(Elevator elevator){
-        System.out.println(elevator.getName() + " on " + elevator.getFloor().getFloorNumber()+" floor, direction:"+elevator.getCurrentDirection());
+    public static void print(LinkedBlockingQueue<Task> tasks) {
+        for (Task task : tasks) {
+            print(task);
+        }
+        System.out.println();
+    }
+
+    public static void showElevator(Elevator elevator) {
+        System.out.println(elevator.getName() + " on " + elevator.getFloor().getFloorNumber() + " floor, direction:" + elevator.getCurrentDirection());
         System.out.print("Tasks: ");
         print(elevator.getTasks());
         print(elevator.getPeople());
         System.out.println("#############################################################");
     }
 
-    public static void buildingCreationMessage(int nrOfFloors, int nrOfElevators) {
-        System.out.println("Building: " + nrOfFloors + " floors and " + nrOfElevators + " elevators");
-    }
-
-    public static void personCreationMessage(int floor, int destinationFloor) {
-        System.out.println("Created person " + floor + " => " + destinationFloor);
-    }
-
-    public static void print(LinkedBlockingQueue<Task> tasks){
-        for(Task task : tasks){
-            print(task);
+    public static void showFloors() {
+        Building kolejowa = Building.getBuilding();
+        Floor[] floors = kolejowa.getFloors();
+        for (int i = floors.length - 1; i >= 0; i--) {
+            for (Elevator elevator : kolejowa.getElevators()) {
+                if (elevator.getFloor().getFloorNumber() == floors[i].getFloorNumber())
+                    System.out.print("#");
+            }
+            System.out.println("\tFloor " + floors[i].getFloorNumber() + " Up:" + floors[i].getUpQueue()
+                    + " Down:" + floors[i].getDownQueue() + " Transported: " + floors[i].getTransportedPeople());
         }
-        System.out.println();
+    }
+
+    public static void transportedPersonMessage(Person person, Elevator elevator) {
+        System.out.println("Person " + person.getName() + " successfully transported to floor " + person.getDestinationFloor() + " by " + elevator.getName() + ".");
     }
 }
