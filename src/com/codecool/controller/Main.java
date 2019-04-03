f47b56c0e558734645259390907d1dbe94f1f26d
Arkadiusz Szreiber <aszreiber1891@gmail.com>
2019-04-03 10:05:50 +0200

    blank window

--- src/com/codecool/controller/Main.java
+++ src/com/codecool/controller/Main.java
@@ -1,5 +1,32 @@
 package com.codecool.controller;
 
-public class Main {
 
+import javafx.geometry.Rectangle2D;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.Screen;
+
+public class Main extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Elevator Simulator");
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout, 1300, 740);
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+
+    }
 }
